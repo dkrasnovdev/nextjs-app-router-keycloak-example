c2d9ea443022a792af3c3b0c331197869e687c4f
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-08-08 20:38:15 +0700

    (feat): handle jwt creation and refreshing

--- src/app/api/auth/[...nexauth]/route.ts
+++ src/app/api/auth/[...nexauth]/route.ts
@@ -1,5 +1,6 @@
-import { logoutRequest } from "@/lib/oidc";
-import type { AuthOptions } from "next-auth";
+import { logoutRequest, refreshTokenRequest } from "@/lib/oidc";
+import type { Account, AuthOptions, User } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import NextAuth from "next-auth/next";
 import { ProviderType } from "next-auth/providers/index";
 import KeycloakProvider from "next-auth/providers/keycloak";
@@ -21,6 +22,57 @@ export const authOptions: AuthOptions = {
   events: {
     async signOut({ token }) {
       await logoutRequest(token.refresh_token);
+    },
+  },
+  callbacks: {
+    async jwt({
+      token,
+      account,
+      user,
+    }: {
+      token: JWT;
+      account: Account | null;
+      user: User | null;
+    }) {
+      // Handle JWT token creation and refreshing
+      if (account && user) {
+        // Update token with account information
+        token.access_token = account.access_token;
+        token.refresh_token = account.refresh_token;
+        token.access_token_expired =
+          Date.now() + (account.expires_in - 15) * 1000;
+        token.refresh_token_expired =
+          Date.now() + (account.refresh_expires_in - 15) * 1000;
+        token.user = user;
+        return token;
+      } else {
+        try {
+          // Send a post request to refresh the token
+          const response = await refreshTokenRequest(token.refresh_token);
+          const tokens = await response.data;
+          if (response.status !== 200) throw tokens;
+          // Update token with refreshed information
+          return {
+            ...token,
+            access_token: tokens.access_token,
+            refresh_token: tokens.refresh_token ?? token.refresh_token,
+            refresh_token_expired:
+              tokens.refresh_expires_in ?? token.refresh_token_expired,
+            expires_in: Math.floor(Date.now() / 1000 + tokens.expires_in),
+            error: null,
+          };
+        } catch (e) {
+          return null as unknown as JWT;
+        }
+      }
+    },
+    // Handle session information
+    async session({ session, token }) {
+      // Update session with user and access token information
+      session.user = token.user;
+      session.error = token.error;
+      session.access_token = token.access_token;
+      return session;
     },
   },
 };
